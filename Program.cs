23af4072aea0bd80e849c6bb9ba5d8376cd622a3
vasud <vasud@LAPTOP-O3D1H17F>
2021-04-30 00:11:31 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static bool done;
+        static readonly object locker = new object();
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                new Thread(Go).Start();
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                int temp = i;
+                new Thread(() => Console.WriteLine(temp)).Start();
+            }
+        }
+
+        static void Go()
+        {
+           lock (locker)
+            {
+                if (!done) { Console.WriteLine("Done"); done = true; }
+            }
+        }
+
+    }
+
+}
